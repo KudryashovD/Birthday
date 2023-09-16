a3d0d2540cccbb4ffd807a48857d5cb672036783
dimacyber <dimacyber@mail.ru>
2023-09-16 19:24:31 +0300

    GoroscopeUpdate

--- UserBirthday/MainWindow.xaml.cs
+++ UserBirthday/MainWindow.xaml.cs
@@ -28,13 +28,26 @@ namespace UserBirthday
 
         private void Count(object sender, RoutedEventArgs e)
         {
-            DateTime date = DP.DisplayDate;
+            DateTime date = (DateTime)DP.SelectedDate;
             DateTime today = DateTime.Now;
+            bool flag = true;
+            while (flag)
+            {
+            if (date > today)
+            {
+                    MessageBox.Show("Дата рождения не может быть больше сегодняшней даты, повторите ввод");
+                    flag = false;
+            }
+                else
+                { 
             TimeSpan difference = today - date;
             Years.Text = Convert.ToString((int)(difference.Days / 365.25));
             Months.Text = Convert.ToString((int)((difference.Days % 365.25) / 30.44));
             Days.Text = Convert.ToString(difference.Days % 30);
+                    flag = false;
+                }
         }
+    }
 
         private void RadioButtonTime_Click(object sender, RoutedEventArgs e)
         {
@@ -44,27 +57,41 @@ namespace UserBirthday
             Week_Button.Visibility = Visibility.Collapsed;
             VisokosYears.Visibility = Visibility.Collapsed;
             Visokos_Button.Visibility = Visibility.Collapsed;
+            Goroscope.Visibility = Visibility.Collapsed;
         }
 
         private void DayOfWeekRepeat(object sender, RoutedEventArgs e)
         {
-            DateTime date = DP.DisplayDate;
+            DateTime date = (DateTime)DP.SelectedDate;
             DateTime today = DateTime.Now;
-            DateTime dayofweek;
-            int Birthdays = 0;
-            for (int year = date.Year; year <= today.Year; year++)
+            bool flag = true;
+            while (flag)
             {
-                for (int month = 1; month <= 12; month++)
+                if (date > today)
                 {
-                    dayofweek = new DateTime(year, month, date.Day);
-                    if (date.DayOfWeek == dayofweek.DayOfWeek && date.Month == dayofweek.Month)
+                    MessageBox.Show("Дата рождения не может быть больше сегодняшней даты, повторите ввод");
+                    flag = false;
+                }
+                else
+                {
+                    DateTime dayofweek;
+                    int Birthdays = 0;
+                    for (int year = date.Year; year <= today.Year; year++)
                     {
-                        Birthdays++;
+                        for (int month = 1; month <= 12; month++)
+                        {
+                            dayofweek = new DateTime(year, month, date.Day);
+                            if (date.DayOfWeek == dayofweek.DayOfWeek && date.Month == dayofweek.Month)
+                            {
+                                Birthdays++;
+                            }
+                        }
                     }
+                    DayOfWeek.Text = Convert.ToString(date.DayOfWeek);
+                    CountDayOfWeek.Text = Convert.ToString(Birthdays);
+                    flag = false;
                 }
             }
-            DayOfWeek.Text = Convert.ToString(date.DayOfWeek);
-            CountDayOfWeek.Text = Convert.ToString(Birthdays);
         }
 
         private void RadioButtonDayOfWeek_Click(object sender, RoutedEventArgs e)
@@ -75,21 +102,35 @@ namespace UserBirthday
             Time_Button.Visibility = Visibility.Collapsed;
             VisokosYears.Visibility = Visibility.Collapsed;
             Visokos_Button.Visibility = Visibility.Collapsed;
+            Goroscope.Visibility = Visibility.Collapsed;
 
         }
         private void VisokosYearRepeat(object sender, RoutedEventArgs e)
         {
-            DateTime date = DP.DisplayDate;
+            DateTime date = (DateTime)DP.SelectedDate;
             DateTime today = DateTime.Now;
-            int LeapYear = 0;
-            for (int year = date.Year; year <= today.Year; year++)
+            bool flag = true;
+            while (flag)
             {
-                if (DateTime.IsLeapYear(year))
+                if (date > today)
                 {
-                    LeapYear++;
+                    MessageBox.Show("Дата рождения не может быть больше сегодняшней даты, повторите ввод");
+                    flag = false;
+                }
+                else
+                {
+                    int LeapYear = 0;
+                    for (int year = date.Year; year <= today.Year; year++)
+                    {
+                        if (DateTime.IsLeapYear(year))
+                        {
+                            LeapYear++;
+                        }
+                    }
+                    VisokosYear.Text = Convert.ToString(LeapYear);
+                    flag = false;
                 }
             }
-            VisokosYear.Text = Convert.ToString(LeapYear);
             }
         private void RadioButtonVisokosYear_Click(object sender, RoutedEventArgs e)
         {
@@ -99,20 +140,169 @@ namespace UserBirthday
             Week_Button.Visibility = Visibility.Collapsed;
             Time.Visibility = Visibility.Collapsed;
             Time_Button.Visibility = Visibility.Collapsed;
+            Goroscope.Visibility = Visibility.Collapsed;
 
         }
 
         private void ConkretVisokosYear(object sender, RoutedEventArgs e)
         {
-            DateTime date = DP.DisplayDate;
+            DateTime date = (DateTime)DP.SelectedDate;
             DateTime today = DateTime.Now;
-            int LeapYear = 0;
-            for (int year = date.Year; year <= today.Year; year++)
+            bool flag = true;
+            while (flag)
             {
-                if (DateTime.IsLeapYear(year))
+                if (date > today)
                 {
-                    MessageBox.Show("Високосный год " + year);
+                    MessageBox.Show("Дата рождения не может быть больше сегодняшней даты, повторите ввод");
+                    flag = false;
                 }
+                else
+                {
+                    for (int year = date.Year; year <= today.Year; year++)
+                    {
+                        if (DateTime.IsLeapYear(year))
+                        {
+                            MessageBox.Show("Високосный год " + year);
+                            flag = false;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void RadioButtonGoroscope(object sender, RoutedEventArgs e)
+        {
+            Goroscope.Visibility = Visibility.Visible;
+            VisokosYears.Visibility = Visibility.Collapsed;
+            Visokos_Button.Visibility = Visibility.Collapsed;
+            DaysOfWeek.Visibility = Visibility.Collapsed;
+            Week_Button.Visibility = Visibility.Collapsed;
+            Time.Visibility = Visibility.Collapsed;
+            Time_Button.Visibility = Visibility.Collapsed;
+        }
+
+        private void ComboBoxGRSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+            
+                    if (CBGoroscope.SelectedIndex == 1)
+                    {
+                        SlavianGR.Visibility = Visibility.Visible;
+                        EastGR.Visibility = Visibility.Collapsed;
+                DateTime date = (DateTime)DP.SelectedDate;
+                    DateTime today = DateTime.Now;
+                    bool flag = true;
+                    while (flag)
+                    {
+                        if (date > today)
+                        {
+                            MessageBox.Show("Дата рождения не может быть больше сегодняшней даты, повторите ввод");
+                            flag = false;
+                        }
+                        else
+                        {
+                            if(date.Day>=24 && date.Month==12 || date.Day<=30 && date.Month == 1)
+                            {
+                                Slavian.Text = "Мороз";
+                            }
+                            else if(date.Day>=31 && date.Month==1 || date.Day<=28 && date.Month == 2)
+                            {
+                                Slavian.Text = "Велес";
+                            }
+                            else if(date.Month == 3)
+                            {
+                                Slavian.Text = "Макошь";
+                            }
+                            else if (date.Month == 4)
+                            {
+                                Slavian.Text = "Жива";
+                            }
+                            else if (date.Day >= 1  && date.Month == 5 || date.Day <= 14 && date.Month == 5)
+                            {
+                                Slavian.Text = "Ярила";
+                            }
+                            else if (date.Day>=15 && date.Month == 5 || date.Day<=2 && date.Month == 6)
+                            {
+                                Slavian.Text = "Леля";
+                            }
+                            else if (date.Day>=2 && date.Month == 6 || date.Day<=12 && date.Month == 6)
+                            {
+                                Slavian.Text = "Кострома";
+                            }
+                            else if (date.Day>=13 && date.Day!=24 && date.Month==6 || date.Day<=6 && date.Month == 7)
+                            {
+                                Slavian.Text = "Додола";
+                            }
+                            else if (date.Day==24 && date.Month == 6)
+                            {
+                                Slavian.Text = "Иван Купала";
+                            }
+                            else if (date.Day>=6 && date.Month==7 || date.Day<=31 && date.Month == 7)
+                            {
+                                Slavian.Text = "Лада";
+                            }
+                            else if (date.Day>=1 && date.Month==8 || date.Day<=28 && date.Month == 8)
+                            {
+                                Slavian.Text = "Перун";
+                            }
+                            else if (date.Day>=29 && date.Month==8 || date.Day<=13 && date.Month == 9)
+                            {
+                                Slavian.Text = "Сева";
+                            }
+                            else if (date.Day>=14 && date.Month==9 || date.Day<=27 && date.Month == 9)
+                            {
+                                Slavian.Text = "Рожаница";
+                            }
+                            else if (date.Day>=28 && date.Month==9 || date.Day<=15 && date.Month == 10)
+                            {
+                                Slavian.Text = "Сварожичи";
+                            }
+                            else if (date.Day>=16 && date.Month==10 || date.Day<=8 && date.Month == 11)
+                            {
+                                Slavian.Text = "Морена";
+                            }
+                            else if(date.Day>=9 && date.Month==11 || date.Day<=28 && date.Month == 11)
+                            {
+                                Slavian.Text = "Зима";
+                            }
+                            else if (date.Day>=29 && date.Month==11 || date.Day<=23 && date.Month == 12)
+                            {
+                                Slavian.Text = "Карачун";
+                            }
+                            flag= false;
+                        }
+                    }
+                        }
+                    else if (CBGoroscope.SelectedIndex == 2)
+                    {
+                        EastGR.Visibility = Visibility.Visible;
+                        SlavianGR.Visibility = Visibility.Collapsed;
+                DateTime date = (DateTime)DP.SelectedDate;
+                    DateTime today = DateTime.Now;
+                    bool flag = true;
+                    while (flag)
+                    {
+                        if (date > today)
+                        {
+                            MessageBox.Show("Дата рождения не может быть больше сегодняшней даты, повторите ввод");
+                            flag = false;
+                        }
+                        else
+                        {
+                            int year = date.Year;
+                            string[] Animals = new string[]{ "ОБЕЗЬЯНА", "ПЕТУХ", "СОБАКА", "СВИНЬЯ", "КРЫСА", "БЫК", "ТИГР", "КРОЛИК", "ДРАКОН", "ЗМЕЯ", "ЛОШАДЬ", "ОВЦА" };
+                            string animal = Animals[year % 12];
+                            East.Text = animal;
+                            flag = false;
+                        }
+                    }
+                        }
+
+            }
+            catch
+            {
+                MessageBox.Show("Введите дату");
             }
         }
     }
